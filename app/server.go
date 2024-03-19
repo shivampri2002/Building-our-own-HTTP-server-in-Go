b884ecd7f91880ef9cb61c6c66a7d5db8452b103
shivam <shivprishi@gmail.com>
2024-03-19 22:58:30 +0530

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -12,16 +12,40 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	_, err = l.Accept()
+
+	conn, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	buffer := make([]byte, 1024)
+
+	_, err = conn.Read(buffer)
+
+	if err != nil {
+		fmt.Println("Error reading the data from the connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	httpResponse := "HTTP/1.1 200 OK\r\n\r\n"
+
+	_, err = conn.Write([]byte(httpResponse))
+
+	if err != nil {
+		fmt.Println("Error writing to the connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	defer l.Close()
 }
